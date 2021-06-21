1be9ba6860458e2eeeab9c22a3d00e528a6ad28b
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-21 14:54:33 -0400

    csv working, need header names

--- converter_no_csv_module.py
+++ converter_no_csv_module.py
@@ -1,4 +1,3 @@
-# import csv
 import json
 from shlex import shlex
 
@@ -8,23 +7,32 @@ json_file_path='readmissions2.json'
 def csv_to_json(csv_path, json_path):
 
     readmissions=[]
+    line_count = 0
     # if headers:
     #     columns = next(reader)
 
     for line in open(csv_file_path, 'r').readlines()[1:]:
 
-        line=line.replace('\n', '').split(',', '|')
+        line=line.replace('\n', '').split(',')
         line_data={}
-#         for i in range(len(line)):
-#           # set key names
-#           if headers:
-#             line_key = columns[i].lower()
-#           else: 
-#             line_key = i
-#           # set key/value
-#           line_data[line_key] = line[i]
-#         # add data to json store 
-#         readmissions.append(line_data)
+    #     if line_count == 0:
+    #         print ("1st line")
+    #         line_count += 1
+    #     else:
+    #         # print ("next line")
+    #         line_count += 1
+    # print(line)
+
+        for i in range(len(line)):
+          # set key names
+          if line_count!=0:
+            line_key = line[i].lower()
+          else: 
+            line_key = i
+          # set key/value
+          line_data[line_key] = line[i]
+        # add data to json store 
+        readmissions.append(line_data)
 
 # def string_and_integer()
 
