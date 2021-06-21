e91096b01e45bacf00528977db162698c1f5802a
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-21 05:42:06 -0400

    working converter

--- converter.py
+++ converter.py
@@ -4,15 +4,16 @@ import json
 csv_file_path='readmissions_df.csv'
 json_file_path='readmission.json'
 
-readmissions={}
+def csv_to_json(csv_path, json_path):
 
-with open(csv_file_path, 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for rows in csv_reader:
-        i=rows['i']
-        readmissions[i]=rows
+    readmissions=[]
 
-with open(json_file_path, 'w') as json_file:
-    json_file.write(json.dumps(readmissions, indent=4))
+    with open(csv_file_path, encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            readmissions.append(row)
 
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json_file.write(json.dumps(readmissions, indent=4))
 
+csv_to_json(csv_file_path, json_file_path)
