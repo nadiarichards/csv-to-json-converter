1db39c7bffc330e8fc02e10c1b0370b4c8cb2682
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-21 06:10:33 -0400

    list of dicts

--- converter_no_csv_module.py
+++ converter_no_csv_module.py
@@ -8,7 +8,16 @@ def csv_to_json(csv_path, json_path):
 
     readmissions={}
 
-    
+    for line in open(csv_file_path, 'r').readlines()[1:]:
+        line=line.replace('\n', '')
+        line.split(',', '|')
+
+    for item in readmissions:
+        for key, value in item.iteritems():
+            try:
+                item[key] = int(value)
+            except ValueError:
+                item[key] = str(value)
 
     with open(json_file_path, 'w', encoding='utf-8') as json_file:
         json_file.write(json.dumps(readmissions, indent=4))
