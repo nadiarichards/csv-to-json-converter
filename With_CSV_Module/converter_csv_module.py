6a0d3c4d97f3d2b4c396efda38594a0a73fc7eea
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-22 00:56:51 -0400

    delimiters

--- With_CSV_Module/converter_csv_module.py
+++ With_CSV_Module/converter_csv_module.py
@@ -1,8 +1,5 @@
 import csv
 import json
-import re
-# import ast
-# from itertools import islice
 
 csv_file_path='readmissions_df.csv'
 json_file_path='readmission.json'
@@ -10,14 +7,12 @@ json_file_path='readmission.json'
 def csv_to_json(csv_path, json_path):
 
     readmissions=[]
-    # str_delimiter=(str_delimiter "|", ",")
 
     with open(csv_file_path, encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         for row in csv_reader:
-            row_data = re.split(',|', row)
-            readmissions.append(row_data)
+            readmissions.append(row)
             
         for item in readmissions:
             for key, value in item.items():
@@ -30,4 +25,4 @@ def csv_to_json(csv_path, json_path):
     with open(json_file_path, 'w', encoding='utf-8') as json_file:
         json_file.write(json.dumps(readmissions, indent=4))
 
-csv_to_json(csv_file_path, json_file_path,)
+csv_to_json(csv_file_path, json_file_path)
