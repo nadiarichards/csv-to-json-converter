35773be61f7a7dbd7d09c6a813f6b5f163a8f19d
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-23 15:21:39 -0400

    hospitals

--- No_CSV_Module/converter_no_csv_module.py
+++ No_CSV_Module/converter_no_csv_module.py
@@ -4,17 +4,21 @@ from shlex import shlex
 csv_file_path='readmissions_df.csv'
 json_file_path='readmissions2.json'
 
+readmissions=[]
+
 def csv_to_json(csv_path, json_path):
 
-    readmissions=[]
-    line_count = 0
     # if headers:
     #     columns = next(reader)
 
     for line in open(csv_file_path, 'r').readlines()[1:]:
 
+# Words is the last line of the doc as a list of objects[]
       words=line.replace('\n', '').split(',')
+      # line_count=(len(line))
+      line_count=0
       line_data={}
+      # print(line[0])
 
         # line = line.rstrip()
         # line = line.replace('"', '')
@@ -26,24 +30,26 @@ def csv_to_json(csv_path, json_path):
       #   line_data[key] = values
       # readmissions.append(line_data)
 
-      print(words)
+      # print(words[0])
 
-    #     if line_count == 0:
-    #         print ("1st line")
-    #         line_count += 1
-    #     else:
-    #         # print ("next line")
-    #         line_count += 1
-    # print(line)
+      if line_count == 0:
+          print ("1st line", line)
+          line_count += 1
+      else:
+          print ("next line")
+          line_count += 1
+          print(line)
+    print(line_count)
 
-    #   for i in range(len(words)):
+    #   for i in range(line_count):
     #         # set key names
     #     if line_count!=0:
-    #       line_key = words[i].lower()
+    #       line_key = line[i].lower()
     #     else: 
     #       line_key = i
+    #       print(line)
     #         # set key/value
-    #     line_data[line_key] = words[i]
+    #     line_data[line_key] = line[i]
     #       # add data to json store 
     # readmissions.append(line_data)
 
@@ -56,7 +62,7 @@ def csv_to_json(csv_path, json_path):
 #             except ValueError:
 #                 item[key] = str(value)
 
-#     with open(json_file_path, 'w', encoding='utf-8') as json_file:
-#         json_file.write(json.dumps(readmissions, indent=4))
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json_file.write(json.dumps(readmissions, indent=4))
 
-# csv_to_json(csv_file_path, json_file_path)
+csv_to_json(csv_file_path, json_file_path)
